e0137107b2501d06dba7f7d736138123a2c2b387
MrUstoz <Tahsimuz@gmail.com>
2024-08-30 22:52:43 +0500

    Pagination ...

---
+++ Managment.System.BLL/Helpers/PaginatedList.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Managment.System.BLL.Helpers;
+
+public class PaginatedList<T>
+{
+    public int PageNumber { get; private set; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+    public int TotalPages { get; private set; }
+    public List<T> Items { get; set; }
+    public PaginatedList(List<T> items, int pageNumber, int pageSize, int count)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = count;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        Items = items;
+    }
+
+    public static async Task<PaginatedList<T>> ToPaginatedList(IQueryable<T> source,  int pageNumber, int pageSize)
+    {
+        var count = await source.CountAsync();
+        var items = await source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PaginatedList<T>(items, count, pageNumber, pageSize);
+    }
+}

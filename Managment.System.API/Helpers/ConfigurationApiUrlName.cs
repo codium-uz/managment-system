34822c5ed9986fcaabb9d7ea29aa1c216d12b629
MrUstoz <Tahsimuz@gmail.com>
2024-08-30 23:11:19 +0500

    ...

---
+++ Managment.System.API/Helpers/ConfigurationApiUrlName.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Managment.System.API.Helpers;
+
+public class ConfigurationApiUrlName : IOutboundParameterTransformer
+{
+    public string TransformOutbound(object value)
+        => value == null ? null : Regex.Replace(value.ToString(), "([a-z])([A-Z])", "$1-$2").ToLower();
+}
--- Managment.System.API/Program.cs
+++ Managment.System.API/Program.cs
@@ -1,8 +1,16 @@
 using Managment.System.API.Extensions;
+using Managment.System.API.Helpers;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Conventions.Add(
+        new RouteTokenTransformerConvention(
+            new ConfigurationApiUrlName()));
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddContext(builder.Configuration);
